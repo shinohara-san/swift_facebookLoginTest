c2e2c752ff36a4cd560ecde0173400e4f8b070d5
shinohara-san <someiyoshino0809@gmail.com>
2020-06-10 13:39:53 +0900

    first commmit

--- fbLoginTest/ViewController.swift
+++ fbLoginTest/ViewController.swift
@@ -1,20 +1,53 @@
-//
-//  ViewController.swift
-//  fbLoginTest
-//
-//  Created by Yuki Shinohara on 2020/06/06.
-//  Copyright © 2020 Yuki Shinohara. All rights reserved.
-//
 
-import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let token = AccessToken.current, !token.isExpired {
+            // User is logged in, do work such as go to next view controller.
+            let token = token.tokenString
+            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                     parameters: ["fields": "email, name"],
+                                                     tokenString: token,
+                                                     version: nil,
+                                                     httpMethod: .get)
+            request.start { (connection, result, error) in
+                print("\(result)")
+//                let vc = self.storyboard?.instantiateViewController(identifier: "AfterLogin") as! AfterLoginViewController
+//                vc.title = "New task"
+//                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            
+        } else {
+            let loginButton = FBLoginButton()
+            loginButton.center = view.center
+            loginButton.delegate = self
+            loginButton.permissions = ["public_profile", "email"]
+            view.addSubview(loginButton)
+        }
     }
-
-
+    
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        let token = result?.token?.tokenString
+        let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                 parameters: ["fields": "email, name"],
+                                                 tokenString: token,
+                                                 version: nil,
+                                                 httpMethod: .get)
+        request.start { (connection, result, error) in
+            print("\(result)")
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
+    
 }
 
+
+// Swift override func viewDidLoad() { super.viewDidLoad()  }
+    
